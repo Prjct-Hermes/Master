ac3b664070b5214d438fb88d9963d57fdf6f2990
Scott Hendrickson <Scott Hendrickson>
2016-07-18 17:13:29 -0600

    Made order cart with totals and fixed the schema so that it accets total and prices.

--- Public/app/routes/orders/ordersCtrl.js
+++ Public/app/routes/orders/ordersCtrl.js
@@ -18,55 +18,71 @@ $scope.recipes = function(){
 // }();
 
 $scope.allRecipes = {};
-$scope.order ={};
+$scope.order ={
+  userId: "",
+  date: "",
+  recipes: [],
+  total: 0,
+};
 
 
-$scope.incrementOrder = function(name, id){
+$scope.incrementOrder = function(recipe){
   console.log("1");
   if (!$scope.order.userId){
     console.log("2");
     $scope.order.userId = $scope.user;
     $scope.order.date = new Date();
     $scope.order.recipes = [];
-    $scope.order.recipes.push({"name": name,"id": id,"quantity": 1});
+    $scope.order.total = recipe.price;
+    return $scope.order.recipes.push({"name": recipe.name,"id": recipe._id,"quantity": 1, "price": recipe.price});
   };
   if($scope.order.recipes.length < 1){
     console.log("3")
-    $scope.order.recipes.push({"name": name,"id": id,"quantity": 1});
+    $scope.order.recipes.push({"name": recipe.name,"id": recipe._id,"quantity": 1, "price": recipe.price});
   }else {
     console.log("4");
     var check = true;
     for (var i = 0; i < $scope.order.recipes.length; i++){
-      if ($scope.order.recipes[i].id === id){
+      if ($scope.order.recipes[i].id === recipe._id){
         console.log("5");
         check = false;
         $scope.order.recipes[i].quantity++;
+        $scope.order.recipes[i].price = parseFloat(recipe.price) * parseFloat($scope.order.recipes[i].quantity);
       }
     }
     if(check){
       console.log("6");
-      $scope.order.recipes.push({"name": name,"id": id,"quantity": 1});
+      $scope.order.recipes.push({"name": recipe.name,"id": recipe._id,"quantity": 1, "price": recipe.price});
     }
+  }
+  $scope.order.total = 0;
+  for (var i = 0; i < $scope.order.recipes.length; i++){
+    $scope.order.total = parseFloat($scope.order.total) + parseFloat($scope.order.recipes[i].price);
   }
   console.log("7", $scope.order);
 }
 
-$scope.decrementOrder = function(name, id){
+$scope.decrementOrder = function(recipe){
   console.log("1");
   for (var i = 0; i < $scope.order.recipes.length; i++){
-    if ($scope.order.recipes[i].id === id){
-      console.log("2", $scope.order);
+    if ($scope.order.recipes[i].id === recipe._id){
+      console.log("2");
       $scope.order.recipes[i].quantity--;
+      $scope.order.recipes[i].price = parseFloat(recipe.price) * parseFloat($scope.order.recipes[i].quantity);
       if ($scope.order.recipes[i].quantity < 1){
-        $scope.order.recipes.splice(i)
+        $scope.order.recipes.splice(i, 1)
       }
     }
+  }
+  $scope.order.total = 0;
+  for (var i = 0; i < $scope.order.recipes.length; i++){
+    $scope.order.total = parseFloat($scope.order.total) + parseFloat($scope.order.recipes[i].price);
   }
   console.log("3", $scope.order);
 }
 
 $scope.createOrder = function(){
-  console.log("creating order?");
+  console.log("creating order");
   mainService.createOrders($scope.order);
   console.log("Order created");
 }
