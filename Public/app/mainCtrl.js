92e5f07f202fa4a7c60d8cf21805c2c397ae7816
Dale Payne <dalebird88@gmail.com>
2016-07-22 10:31:04 -0600

    start

--- Public/app/mainCtrl.js
+++ Public/app/mainCtrl.js
@@ -5,5 +5,11 @@ angular.module('hermes').controller('mainCtrl', function($scope, $state, mainSer
     })
   };
 
+  $scope.retrieveAlerts =  function(){
+    $scope.alerts = mainService.retrieveAlerts();
+    $scope.alertLength = $scope.alerts.length;
+    console.log($scope.alertLength)
+  };
+  $scope.retrieveAlerts();
 
 });
