2ab3c86f0b6bd4d0e850c4167cf6d86f931d61c1
Aaron Chesley <FlashBanistan66@gmail.com>
2016-07-25 09:20:21 -0600

    no real changes just moved things around

--- Public/app/directives/header/headerCtrl.js
+++ Public/app/directives/header/headerCtrl.js
@@ -2,17 +2,14 @@ angular.module('hermes').controller('headerCtrl',function($scope, mainService){
 
   var getUser = mainService.getCurrentUser().then(function(response){
     $scope.user = response.data._id;
+    $scope.alertItems = mainService.retrieveStockItems();
 
-    (function(){
-      $scope.alertItems = mainService.retrieveStockItems();
-    })();
 
     $scope.alerts = [];
 
     (function(){
       for (var i = 0; i < $scope.alertItems.length; i++){
         if($scope.alertItems[i].quantity <= $scope.alertItems[i].alertQuantity){
-
           var alert = {
             name: $scope.alertItems[i].name,
             id: $scope.alertItems[i]._id,
@@ -23,11 +20,54 @@ angular.module('hermes').controller('headerCtrl',function($scope, mainService){
         }
       }
       $scope.alertLength = $scope.alerts.length;
-
       mainService.createAlerts($scope.alerts);
     })();
-
-
   });
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// angular.module('hermes').controller('headerCtrl',function($scope, mainService){
+//
+//   var getUser = mainService.getCurrentUser().then(function(response){
+//     $scope.user = response.data._id;
+//
+//     (function(){
+//       $scope.alertItems = mainService.retrieveStockItems();
+//     })();
+//
+//     $scope.alerts = [];
+//
+//     (function(){
+//       for (var i = 0; i < $scope.alertItems.length; i++){
+//         if($scope.alertItems[i].quantity <= $scope.alertItems[i].alertQuantity){
+//           var alert = {
+//             name: $scope.alertItems[i].name,
+//             id: $scope.alertItems[i]._id,
+//             quantity: $scope.alertItems[i].quantity,
+//             alertQuantity: $scope.alertItems[i].alertQuantity
+//           }
+//           $scope.alerts.push(angular.copy(alert));
+//         }
+//       }
+//       $scope.alertLength = $scope.alerts.length;
+//       mainService.createAlerts($scope.alerts);
+//     })();
+//   });
+// });
