f8d99c16283cb2154aa46115eda3e91b86cc8a4e
Scott Hendrickson <Scott Hendrickson>
2016-07-29 14:22:10 -0600

    History now has the ability to update within a date range as long as you are DECREASING the number of bars. Trying to go back breaks everything. Also the x axis isn't populating dates for any of the changes. YAY!

--- Public/app/routes/history/historyCtrl.js
+++ Public/app/routes/history/historyCtrl.js
@@ -1,14 +1,46 @@
 angular.module('hermes').controller('historyCtrl', function($scope, mainService, user, orders){
-// $scope.getData = function(){
-//   mainService.getDataOrders().then(function(response){
-//     $scope.allOrders = mainService.retrieveAllOrders();
-//     $scope.data();
-//   })
-//   $scope.allOrders = mainService.retrieveAllOrders();
-// }();
+
 $scope.allOrders = orders
-//Get all orders to combine into a single day
-//Create an array that is {day, qty}
+$scope.minDate = "";
+$scope.maxDate = "";
+var dateRange = function(){
+  if (!$scope.minDate){
+    $scope.minDate = new Date($scope.allOrders[0].date).getTime()
+  }
+  if (!$scope.maxDate){
+    $scope.minDate = new Date($scope.allOrders[0].date).getTime()
+  }
+  for (var i = 1; i < $scope.allOrders.length; i++){
+    var date = new Date($scope.allOrders[i].date).getTime();
+    if(date < $scope.minDate){
+      $scope.minDate = new Date(date);
+    }
+    if(date > $scope.maxDate){
+      $scope.maxDate = new Date(date);
+    }
+  }
+  console.log("Min: ", $scope.minDate, "Max: ", $scope.maxDate);
+}();
+$scope.setDateRange = function(){
+  $scope.tempData = [];
+  for (var i = 1; i < $scope.allOrders.length; i++){
+    var date = new Date($scope.allOrders[i].date).getTime();
+    if(date > $scope.minDate && date < $scope.maxDate){
+      var date = new Date($scope.allOrders[i].date);
+      var year = date.getFullYear();
+      var month = date.getMonth();
+      var day = date.getDay();
+      var hour = date.getHours();
+      var minutes = date.getMinutes();
+      var newDate = year + "-" + month + "-"+ day + " " + hour + ":" + minutes;
+      var obj = {};
+      obj.date = newDate;
+      obj.quantity = $scope.allOrders[i].quantityTotal;
+      $scope.tempData.push(obj);
+    }
+  }
+  console.log($scope.tempData)
+}
 $scope.tempData = [];
 $scope.data = function(){
   for (var i = 0; i < $scope.allOrders.length; i++){
@@ -16,7 +48,9 @@ $scope.data = function(){
     var year = date.getFullYear();
     var month = date.getMonth();
     var day = date.getDay();
-    var newDate = year + "-" + month + "-"+ day;
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var newDate = year + "-" + month + "-"+ day + " " + hour + ":" + minutes;
     var obj = {};
     obj.date = newDate;
     obj.quantity = $scope.allOrders[i].quantityTotal;
@@ -79,7 +113,7 @@ var tip = d3.tip()
     return "<strong>Quantity:</strong> <span style='color:red'>" + d.quantity + "</span>";
   })
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".history-data").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -87,75 +121,20 @@ var svg = d3.select("body").append("svg")
 
 svg.call(tip);
 
-// The new data variable.
-var data = [{date: "2016-07-01T21:39:56.537Z", quantity: 5 },
-    {date: "2016-07-02T21:39:56.537Z", quantity: 11 },
-    {date: "2016-07-02T21:39:56.537Z", quantity: 10 },
-    {date: "2016-07-04T21:48:36.885Z", quantity: 23 },
-    {date: "2016-07-05T19:58:14.297Z", quantity: 6 },
-    {date: "2016-07-06T19:58:53.291Z", quantity: 2 },
-    {date: "2016-07-07T20:00:42.465Z", quantity: 3 },
-    {date: "2016-07-08T20:04:15.827Z", quantity: 3 },
-    {date: "2016-07-09T20:05:45.491Z", quantity: 2 },
-    {date: "2016-07-09T20:05:45.491Z", quantity: 2 },
-    {date: "2016-07-11T20:11:11.789Z", quantity: 1 },
-    {date: "2016-07-12T20:13:41.855Z", quantity: 1 },
-    {date: "2016-07-13T20:22:22.529Z", quantity: 1 },
-    {date: "2016-07-14T20:25:51.932Z", quantity: 1 },
-    {date: "2016-07-15T20:26:29.642Z", quantity: 1 },
-    {date: "2016-07-16T20:28:28.710Z", quantity: 1 },
-    {date: "2016-07-16T20:28:28.710Z", quantity: 1 },
-    {date: "2016-07-18T20:31:34.193Z", quantity: 1 },
-    {date: "2016-07-19T20:36:32.218Z", quantity: 1 },
-    {date: "2016-07-20T20:36:58.054Z", quantity: 1 },
-    {date: "2016-07-21T20:37:05.060Z", quantity: 1 },
-    {date: "2016-07-22T20:39:44.477Z", quantity: 1 },
-    {date: "2016-07-23T20:39:47.528Z", quantity: 1 },
-    {date: "2016-07-23T20:39:47.528Z", quantity: 1 },
-    {date: "2016-07-25T20:41:12.045Z", quantity: 1 },
-    {date: "2016-07-26T20:41:16.394Z", quantity: 1 },
-    {date: "2016-07-27T20:41:43.656Z", quantity: 1 },
-    {date: "2016-07-28T20:42:47.115Z", quantity: 1 },
-    {date: "2016-07-29T20:44:04.225Z", quantity: 1 },
-    {date: "2016-07-29T20:44:04.225Z", quantity: 1 },
-    {date: "2016-07-29T20:44:04.225Z", quantity: 1 },
-    {date: "2016-07-01T20:56:01.559Z", quantity: 1 },
-    {date: "2016-07-02T22:03:26.052Z", quantity: 1 },
-    {date: "2016-07-02T22:03:26.052Z", quantity: 1 },
-    {date: "2016-07-04T22:08:20.442Z", quantity: 1 },
-    {date: "2016-07-05T22:09:10.667Z", quantity: 1 },
-    {date: "2016-07-15T22:10:26.000Z", quantity: 1 },
-    {date: "2016-07-13T22:11:05.980Z", quantity: 1 },
-    {date: "2016-07-12T22:11:23.191Z", quantity: 1 },
-    {date: "2016-07-11T22:11:27.330Z", quantity: 12 },
-    {date: "2016-07-11T22:12:30.093Z", quantity: 1 },
-    {date: "2016-07-11T22:12:30.093Z", quantity: 1 },
-    {date: "2016-07-09T22:14:22.896Z", quantity: 2 },
-    {date: "2016-07-19T22:16:44.279Z", quantity: 1 },
-    {date: "2016-07-08T22:16:48.677Z", quantity: 7 },
-    {date: "2016-07-07T22:23:02.680Z", quantity: 1 },
-    {date: "2016-07-07T22:24:40.575Z", quantity: 1 },
-    {date: "2016-07-06T22:24:44.772Z", quantity: 3 },
-    {date: "2016-07-05T22:33:29.545Z", quantity: 3 },
-    {date: "2016-07-05T22:33:29.545Z", quantity: 4 },
-    {date: "2016-07-04T22:23:06.375Z", quantity: 4 },
-    {date: "2016-07-02T22:23:06.375Z", quantity: 4 },
-    {date: "2016-07-01T22:23:06.375Z", quantity: 4 },
-    {date: "2016-07-22T21:30:21.959Z", quantity: 3 },
-    {date: "2016-07-22T21:34:43.083Z", quantity: 1 },
-    {date: "2016-07-25T21:00:33.926Z", quantity: 1 },
-    {date: "2016-07-25T21:06:10.677Z", quantity: 1 },
-    {date: "2016-07-27T17:15:00.871Z", quantity: 4 },
-    {date: "2016-07-27T17:59:27.879Z", quantity: 1 }];
 
 // The following code was contained in the callback function.
-x.domain(data.map(function(d) { return d.date; }));
-y.domain([0, d3.max(data, function(d) { return d.quantity; })]);
+x.domain($scope.tempData.map(function(d) { return d.date; }));
+y.domain([0, d3.max($scope.tempData, function(d) { return d.quantity; })]);
 
 svg.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+    .call(xAxis)
+  .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", "-.55em")
+    .attr("transform", "rotate(-90)" );
 
 svg.append("g")
     .attr("class", "y axis")
@@ -178,11 +157,43 @@ svg.selectAll(".bar")
     .on('mouseover', tip.show)
     .on('mouseout', tip.hide)
 
+
+
 function type(d) {
   d.quantity = +d.quantity;
   return d;
 }
 
+
+ $scope.updateData = function() {
+    	// Scale the range of the data again
+      x.domain($scope.tempData.map(function(d) { return d.date; }));
+      y.domain([0, d3.max($scope.tempData, function(d) { return d.quantity; })]);
+
+    // Select the section we want to apply our changes to
+    var bars = d3.select(".history-data").selectAll(".bar").data($scope.tempData, function(d) { return d.date; });
+
+    // Make the changes
+    bars.exit()
+    .transition()
+      .duration(300)
+    .attr("y", y(0))
+    .attr("height", height - y(0))
+    .style('fill-opacity', 1e-6)
+    .remove();
+
+    bars.enter().append("rect")
+    .attr("class", "bar")
+    .attr("y", y(0))
+    .attr("height", height - y(0));
+
+    bars.transition().duration(300).attr("x", function(d) { return x(d.date); }) // (d) is one item from the data array, x is the scale object from above
+    .attr("width", x.rangeBand()) // constant, so no callback function(d) here
+    .attr("y", function(d) { return y(d.quantity); })
+    .attr("height", function(d) { return height - y(d.quantity); }); // flip the height, because y's domain is bottom up, but SVG renders top down
+
+
+    };
 
 // //this block of code successfully display's everthing but not formatted correctly.
 //
